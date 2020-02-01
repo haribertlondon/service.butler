3a97e0279031de1fff6b685c7bed2f4ea687ece7
haribertlondon <haribertlondon@yahoo.de>
2020-02-01 22:31:15 +0100

    Improved statemachine

--- stateMachine.py
+++ stateMachine.py
@@ -8,18 +8,18 @@ class TimeTracker():
         self.seconds_per_buffer = seconds_per_buffer
         self.reset()
         
-    def reset(self):
-        self.pause_time_after_phrase = 0.0
-        self.total_pause_time = 0.0
+    def reset(self):                
         self.elapsed_time = 0.0
-        self.total_time_since_last_hotword = 0.0    
-        self.pure_phrase_time = 0.0
+        self.time_since_hotword = 0.0    
+        self.pause_time_after_phrase = 0.0
+        self.high_energy_time_since_hotword = 0.0
+        self.high_energy_time = 0.0
         self.startTime_for_tictoc = time.time()
         
     def update(self, infos):
         self.reset()
         for info in infos:
-            self.updateChunk(info)
+            self.updateChunk(info)        
         
     def updateChunk(self, info): 
         (isEnergy, isHotword) = info 
@@ -27,16 +27,17 @@ class TimeTracker():
         seconds = self.seconds_per_buffer  
         self.elapsed_time += seconds
         
-        if (isHotword or self.total_time_since_last_hotword > 0.0):
-            self.total_time_since_last_hotword += seconds
+        if (isHotword or self.time_since_hotword > 0.0):
+            self.time_since_hotword += seconds
+            if isEnergy:
+                self.high_energy_time_since_hotword += seconds
              
         if isEnergy:
-            self.pure_phrase_time += seconds
+            self.high_energy_time += seconds
             self.pause_time_after_phrase = 0                       
         else: 
-            if self.pure_phrase_time  > 0:           #only count if something was said before
-                self.pause_time_after_phrase += seconds
-            self.total_pause_time += seconds            
+            if self.high_energy_time  > 0:           #only count if something was said before
+                self.pause_time_after_phrase += seconds            
 
 
 class HotwordDetectorStateMachine(speech.HotwordDetector): 
@@ -80,12 +81,12 @@ class HotwordDetectorStateMachine(speech.HotwordDetector):
 
         if resultSphinx>0 or resultSnowboy>0:
             self.infos[-1] = (self.infos[-1][0], True) 
-            print("Snowboy="+ str(resultSnowboy) + "  Sphinx=" + str(resultSphinx) + "  Energy= " + str(energy) +"  Threshold=" + str( self.energy_threshold)+ " Time: "+str(self.tracker.pure_phrase_time) +"sec > "+ str(settings.LISTEN_HOTWORD_MIN_DURATION )+"sec")
+            print("Snowboy="+ str(resultSnowboy) + "  Sphinx=" + str(resultSphinx) + "  Energy= " + str(energy) +"  Threshold=" + str( self.energy_threshold)+ " Time: "+str(self.tracker.high_energy_time) +"sec > "+ str(settings.LISTEN_HOTWORD_MIN_DURATION )+"sec")
             if settings.isDebug():
                 self.storeWav()
                 print("Stored to file")
         
-        if (self.tracker.pure_phrase_time > settings.LISTEN_HOTWORD_MIN_DURATION) and (resultSnowboy > 0 or resultSphinx > 0):
+        if (self.tracker.high_energy_time > settings.LISTEN_HOTWORD_MIN_DURATION) and (resultSnowboy > 0 or resultSphinx > 0):
             print("Keyword detected at time: "+ time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), "Snowboy", resultSnowboy, "Sphinx", resultSphinx)
             
             if listening_callback is not None:
@@ -101,14 +102,25 @@ class HotwordDetectorStateMachine(speech.HotwordDetector):
     def state_phrase(self):
 
         if self.tracker.elapsed_time > settings.LISTEN_PHRASE_TOTALTIMEOUT: #reached max time
-                print("MaxTimeLimit reached", self.tracker.elapsed_time, 'Phrase', self.tracker.pure_phrase_time, 'Pause: ', self.tracker.pause_time_after_phrase)
+                print("MaxTimeLimit reached", self.tracker.elapsed_time, 'Phrase', self.tracker.high_energy_time, 'Pause: ', self.tracker.pause_time_after_phrase)
                 return "init"
-        elif self.tracker.pause_time_after_phrase > settings.LISTEN_PHRASE_PAUSE_THRESHOLD and self.tracker.total_time_since_last_hotword > settings.LISTEN_FULLPHRASE_MIN_TIME: #wait for pause at the end
-            if self.tracker.pure_phrase_time > settings.LISTEN_PUREPHRASE_MIN_TIME  and self.tracker.total_time_since_last_hotword > settings.LISTEN_FULLPHRASE_MIN_TIME:
-                print("Pause ok and Phrase     ok  Pause: "+str( self.tracker.pause_time_after_phrase) +' || '+str(self.tracker.pure_phrase_time) + ' > '+str(settings.LISTEN_PUREPHRASE_MIN_TIME  )+'  ||  '+str(self.tracker.total_time_since_last_hotword )+ ' > '+ str(settings.LISTEN_FULLPHRASE_MIN_TIME))                        
+        elif self.tracker.pause_time_after_phrase > settings.LISTEN_PAUSE_TIME_AFTER_PHRASE_THRESHOLD:
+            
+            self.tracker.energy_percentage = (self.tracker.high_energy_time_since_hotword+1e-50) / (self.tracker.time_since_hotword-settings.LISTEN_PAUSE_TIME_AFTER_PHRASE_THRESHOLD+1e-40)
+             
+            checkPause = self.tracker.pause_time_after_phrase > settings.LISTEN_PAUSE_TIME_AFTER_PHRASE_THRESHOLD
+            checkPhrase = self.tracker.high_energy_time_since_hotword > settings.LISTEN_HIGH_ENERGY_TIME_SINCE_HOTWORD_THRESHOLD  
+            checkTotal = self.tracker.time_since_hotword > settings.LISTEN_TIME_SINCE_HOTWORD_THRESHOLD    
+            checkPercentage = self.tracker.energy_percentage > settings.LISTEN_PHRASE_PERCENTAGE
+            
+            print("Checking Pause:  ", checkPause,      '   ', self.tracker.pause_time_after_phrase , '>', settings.LISTEN_PAUSE_TIME_AFTER_PHRASE_THRESHOLD)
+            print("Checking Phrase: ", checkPhrase,     '   ', self.tracker.high_energy_time_since_hotword , '>', settings.LISTEN_HIGH_ENERGY_TIME_SINCE_HOTWORD_THRESHOLD)
+            print("Checking Total:  ", checkTotal,      '   ', self.tracker.time_since_hotword ,'>', settings.LISTEN_TIME_SINCE_HOTWORD_THRESHOLD)
+            print("Checking Percent:", checkPercentage, '   ', self.tracker.energy_percentage ,'>', settings.LISTEN_PHRASE_PERCENTAGE)
+            
+            if checkPhrase and checkTotal and checkPercentage:                
                 return "recognition"
-            else:
-                print("Pause ok and Phrase not ok Pause: "+str( self.tracker.pause_time_after_phrase) +' || '+str(self.tracker.pure_phrase_time) + ' > '+str(settings.LISTEN_PUREPHRASE_MIN_TIME  )+'  ||  '+str(self.tracker.total_time_since_last_hotword )+ ' > '+ str(settings.LISTEN_FULLPHRASE_MIN_TIME))    
+            else:                
                 return "init"    
         else:
             return "phrase" # wait for more pause at the end of phrase
@@ -122,14 +134,15 @@ class HotwordDetectorStateMachine(speech.HotwordDetector):
         print("Starting speech recognition...")
 
         #cut pause at the end of buffer (increases recognition speed)
-        self.cutRightFrames(settings.LISTEN_PHRASE_PAUSE_THRESHOLD-0.2)
+        self.cutRightFrames(settings.LISTEN_PAUSE_TIME_AFTER_PHRASE_THRESHOLD-0.2)
         print("New bufferLen: "+str(len(self.frames)))
         
         response = self.recognize()
         
         print("Detected: ", response["transcription"])
         try:
-            print(response["transcription"].encode("utf8")) #print("Print as utf8 in python 2")
+            if response["transcription"] is not None:
+                print(response["transcription"].encode("utf8")) #print("Print as utf8 in python 2")
         except Exception as e:
             print(e)
 
@@ -187,7 +200,7 @@ class HotwordDetectorStateMachine(speech.HotwordDetector):
                     
                 #print out
                 if lastState != state:# or settings.isDebug():
-                    print('Current state', state, "Buffer: ", len(self.frames), ' Energy:', str(round(energy,2)), '>', str(round(self.energy_threshold,2)), 'Time: ',str(round(time.time() - self.tracker.startTime_for_tictoc,1)), 'Elapsed: ', round(self.tracker.elapsed_time,2), 'Pause: ', round(self.tracker.pause_time_after_phrase,2), 'Phrase: ', round(self.tracker.pure_phrase_time,2) )
+                    print('Current state', state, "Buffer: ", len(self.frames), ' Energy:', str(round(energy,2)), '>', str(round(self.energy_threshold,2)), 'Time: ',str(round(time.time() - self.tracker.startTime_for_tictoc,1)), 'Elapsed: ', round(self.tracker.elapsed_time,2), 'Pause: ', round(self.tracker.pause_time_after_phrase,2), 'Phrase: ', round(self.tracker.high_energy_time,2) )
             except Exception as e:
                 print("Fatal error in state machine. Restart...")
                 print(e)
