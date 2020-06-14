2be7cc9999bf299caa94bbccae0acb58f750a3c6
haribertlondon <haribertlondon@yahoo.de>
2020-06-14 16:17:39 +0200

    New autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -7,49 +7,39 @@
 #wait for boot
 
 echo "Running autostart script"
-
 echo $(date)
-
-echo "I am sleeping for 30sec..."
-sleep 30
-
-echo $(date)
-
-#wait for internet connection
-while ! /sbin/ifconfig eth0 | grep 'inet [0-9]*'; do
-    sleep 3
-done
-
-sleep 5
-
-echo "Found ethernet connection"
-echo $(date)
-
-
-echo "Start kodi"
-echo $(date)
-
 
 if pgrep kodi.*bin.* > /dev/null
 then
-    echo Kodi Found. Not starting
+    echo "Kodi Found"
 else
-    echo Kodi Not Found
-    echo Wait For Network
-    sleep 12
-    echo Now Starting Kodi
+	echo "Kodi Not Found"
+    echo "Wait For Network"
+
+	#wait for internet connection
+	while ! /sbin/ifconfig eth0 | grep 'inet [0-9]*'; do
+		echo "Waiting for ethernet connection...."
+		sleep 3
+	done
+
+	echo "Found ethernet connection"
+	echo $(date)
+
+	echo "I am sleeping for 30sec..."
+	sleep 30
+    
+    echo "Start kodi"	
     kodi --standalone &
-    echo Kodi Started
+    echo "Kodi Started"
 fi
-
-echo "Start service"
-echo $(date)
-
 
 if pgrep -f [^g].*python.*service.* > /dev/null
 then
-    echo Service Found
+    echo "Service Found"
 else
+	echo "Start service"
+	echo $(date)
+
     echo Change to Service Dir
     cd /home/pi/service.butler 
     echo Starting Service
